345dd721ba0bef6810eb424f6799acd9fc86a25e
Bhavya-is-great <147585292+Bhavya-is-great@users.noreply.github.com>
2023-12-30 23:13:41 +0530

    New

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,15 +1,16 @@
-import React , {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import SideBar from './elements/Sidebar'
 import SearchEngine from './elements/SearchEngine'
 import PopularProducts from './elements/PopularProducts';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import Footer from './elements/Footer';
 
 
 
 
 function Home() {
-  useEffect(()=>{
-  if (localStorage.getItem("list")) {
+  useEffect(() => {
+    if (localStorage.getItem("list")) {
 
       // LocalStorage key already exists, so do nothing.
 
@@ -19,8 +20,8 @@ function Home() {
 
       localStorage.setItem("list", JSON.stringify([]));
 
-  }
-  if (localStorage.getItem("listv")) {
+    }
+    if (localStorage.getItem("listv")) {
 
       // LocalStorage key already exists, so do nothing.
 
@@ -30,8 +31,8 @@ function Home() {
 
       localStorage.setItem("listv", JSON.stringify([]));
 
-  }
-  if (localStorage.getItem("listf")) {
+    }
+    if (localStorage.getItem("listf")) {
 
       // LocalStorage key already exists, so do nothing.
 
@@ -41,9 +42,9 @@ function Home() {
 
       localStorage.setItem("listf", JSON.stringify([]));
 
-  }
+    }
 
-  if (localStorage.getItem("pre")) {
+    if (localStorage.getItem("pre")) {
 
       // LocalStorage key already exists, so do nothing.
 
@@ -53,17 +54,32 @@ function Home() {
 
       localStorage.setItem("pre", JSON.stringify([]));
 
-  }
- 
-},[])
+    }
+
+    if (localStorage.getItem("lists")) {
+
+      // LocalStorage key already exists, so do nothing.
+
+    } else {
+
+      // LocalStorage key doesn't exist, so create it.
+
+      localStorage.setItem("pre", JSON.stringify([]));
+
+    }
+
+  }, [])
   const navigate = useNavigate();
   return (
     <div>
       <SideBar />
       <SearchEngine heading="Order your daily Groceries" />
-    <br /><br /><br />
-      <input type="button" value="All items" onClick={()=>navigate('/allitems')} className="orderform send-btn hoverbtn" />
+      <br />
+      <form>
+        <input type="button" value="All items" onClick={() => navigate('/allitems')} className="orderform send-btn hoverbtn" />
+      </form>
       <PopularProducts />
+      {/* <Footer/> */}
     </div>
   )
 }
