359b35ba22ae6b6a23293a1cc6e3de14e71a63d2
Bhavya-is-great <147585292+Bhavya-is-great@users.noreply.github.com>
2023-12-30 16:40:11 +0530

    Sai brothers

--- src/Components/elements/SearchEngine.js
+++ src/Components/elements/SearchEngine.js
@@ -1,10 +1,25 @@
-import React from 'react'
-import bg1 from './images/bg-1.png'
-import bg2 from './images/bg-2.png'
+import React ,{useEffect,useState} from 'react';
+import bg1 from './images/bg-1.png';
+import bg2 from './images/bg-2.png';
 
 
 
 function SearchEngine(props) {
+const [list,setList] = useState([]);
+  const [query,setQuery] = useState([]);
+  useEffect(()=>{
+    axios.post("https://sai-brothers.onrender.com/allitem",{table:"vegetables"})
+    .then(res => {setList(res.data)})
+    .catch(err => console.log(err))
+  },[]);
+
+const handleChange = (e) => {
+  setQuery(e.target.value);
+}
+
+const handleSubmit = () => {}
+
+  useEffect(()=>{console.log(list)},[list]);
   return (
     <section id='search-banner'>
       <img src={bg1} alt="img" className='bg-1' />
@@ -13,9 +28,18 @@ function SearchEngine(props) {
       <div className="search-banner-text">
         <h1>{props.heading}</h1>
         <strong>#Free delivery on purchase of at least 250</strong>
-      <form action="" className='search-box'>
+      <form onSubmit={handleSubmit} className='search-box'>
         <i className='fas fa-search'></i>
-        <input type="text" className='search-input' placeholder='search your daily grocery' name='search' required />
+        <input type="text" onChange={handleChange} list="queryy" value={query} className='search-input' placeholder='search your daily grocery' required />
+    <datalist id="queryy">
+    {
+     list.map((item)) => {
+    return(
+      <option value={item.title} ></option>
+    )
+     })
+    }
+    </datalist>
         <input type="submit" className='search-btn' />
       </form>
       </div>
